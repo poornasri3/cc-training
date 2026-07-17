57ff09566e59a86caaa391c04ecf9ee23217a250
poornasri3 <poornasrinatarajan@gmail.com>
2026-07-17 21:51:46 +0530

    Add files via upload

---
+++ cc.training 4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+ 	int i,j,n;
+ 	printf("Enter the max limit :");
+ 	scanf("%d",&n);
+ 	for(i=0;i<n;i++)
+ 	{
+     	  for(j=0;j<n;j++)
+     	  {
+         	if ( i>=j )
+           	    printf("*");
+            else
+        		printf(" ");
+     	   }
+      	printf("\n");
+	}
+    return 0;  
+}
